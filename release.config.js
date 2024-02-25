19730689db0c55cc514d60b884603621420b1c68
Shane <s9778010@gmail.com>
2024-02-25 15:55:03 +0800

    chore: update release.config.js to add github plugin

--- release.config.js
+++ release.config.js
@@ -1,27 +1,25 @@
-//release.config.js
 module.exports = {
-    // 在什麼分支下，要執行 semantic-release
     branches: ['main'],
     plugins: [
-        '@semantic-release/commit-analyzer',//分析你的 commit message,有沒有 feat or fix
+        '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         ['@semantic-release/npm',{npmPublish: false}],
         [
-            '@semantic-release/changelog', //產生CHANGELOG
+            '@semantic-release/changelog',
             {
                 mangle: false,
                 headerIds: false,
-                changelogFile: 'CHANGELOG.md', //設定檔案名稱
+                changelogFile: 'CHANGELOG.md',
             },
         ],
         [
-            '@semantic-release/git', // 新增CHANGELOG.md後，進行commit
+            '@semantic-release/git',
             {
-                assets: ['CHANGELOG.md', 'package.json'], // commit 要加入的檔案
+                assets: ['CHANGELOG.md', 'package.json'],
                 message:
-                    'chore(release): ${nextRelease.version} [skip ci]', //commit 時的 message
+                    'chore(release): ${nextRelease.version} [skip ci]',
             },
         ],
-
+        '@semantic-release/github',
     ],
 };
